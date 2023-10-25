1bd68fa792eee994ecc2c12f12f3b6895659bb85
GeiOoo <throw3n@gmail.com>
2023-10-25 21:23:36 +0200

    changes

--- src/data/createBranch.ts
+++ src/data/createBranch.ts
@@ -1,4 +1,5 @@
 import * as vscode from 'vscode';
+import { IIssue } from '../view/app/model';
 
 /**
  * createBranch - Creates a branch that defaults to the issue's id and name,
@@ -6,47 +7,33 @@ import * as vscode from 'vscode';
  *
  * @param {string} branchName
  */
-export const createBranch = async (branchName: string) => {
-  const config = vscode.workspace.getConfiguration('git');
+export const createBranch = async (data: IIssue) => {
+	const replaceAll = (value: string, search: string, replace: string) => value.split(search).join(replace);
+	const id = data.idReadable;
 
-  const branchWhitespaceChar = config.get<string>('branchWhitespaceChar')!;
-  const branchValidationRegex = config.get<string>('branchValidationRegex')!;
+	let name = data.summary;
 
-  const sanitize = (name: string) =>
-    name
-      ? name
-          .trim()
-          .replace(/^-+/, '')
-          .replace(/^\.|\/\.|\.\.|~|\^|:|\/$|\.lock$|\.lock\/|\\|\*|\s|^\s*$|\.$|\[|\]$/g, branchWhitespaceChar)
-      : name;
+	name = replaceAll(name, ' ', '-');
+	name = replaceAll(name, ',', '');
+	name = replaceAll(name, ':', '');
+	name = replaceAll(name, ';', '');
+	name = replaceAll(name, '.', '');
+	name = replaceAll(name, 'ä', 'ae');
+	name = replaceAll(name, 'Ä', 'Ae');
+	name = replaceAll(name, 'ö', 'oe');
+	name = replaceAll(name, 'Ö', 'Oe');
+	name = replaceAll(name, 'ü', 'ue');
+	name = replaceAll(name, 'Ü', 'Ue');
+	name = replaceAll(name, 'ß', 'ss');
 
-  const capitalizeBranchName = branchName
-    .replace(/(^\w{1})|(\s+\w{1})/g, (letter) => letter.toUpperCase())
-    .replace(/ /g, '');
+	const appName = data.customFields.find(field => field.name === 'App').value.name.toLowerCase();
+	const type = data.customFields.find(field => field.name === 'Type').value.name.toLowerCase();
 
-  const result = await vscode.window.showInputBox({
-    value: sanitize(capitalizeBranchName).substring(0, 28),
-    prompt: 'Please provide or confirm a new branch name.',
-    placeHolder: 'Enter YouTrack Issue Id (For example: YT-123)',
-    validateInput: (name: string) => {
-      const validateName = new RegExp(branchValidationRegex);
-      if (validateName.test(sanitize(name))) {
-        return null;
-      }
+	const branchName = `${appName}/${type}/${id}-${name}`;
 
-      return `branch name format invalid.`;
-    },
-  });
+	// Create a new terminal and run a git create branch and check it out.
+	const terminal = vscode.window.createTerminal(`YouTrack Git`);
+	terminal.sendText(`git checkout -b ${branchName}`);
 
-  // Check if user did not supply a branch name or escaped out.
-  if (!result) {
-    vscode.window.showInformationMessage('Branch was not created');
-    return;
-  }
-
-  // Create a new terminal and run a git create branch and check it out.
-  const terminal = vscode.window.createTerminal(`YouTrack Git`);
-  terminal.sendText(`git checkout -b ${result}`);
-
-  vscode.window.showInformationMessage(`Branch created: ${result}`);
+	vscode.window.showWarningMessage(`Branch created: ${branchName}`);
 };
